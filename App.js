c9c1c0b011805a1eccb1d3e3808feaf8bbdc6a3c
Danilo <danilojmini2@gmail.com>
2026-01-30 11:46:49 -0300

    Atualizando arquivos

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Danilo está editando o aplicativo</Text>
+      <Text>Podemos editar mexendo nos arquivos</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0000ff',
+    color: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
